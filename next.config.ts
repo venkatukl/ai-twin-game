2752b30aacb8b383f8548bd1a2812d44017a0b96
Venkatesh Nachimuthu <venkatesh.nachimuthu@gmail.com>
2026-06-22 15:47:16 +0530

    Add EsLint

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactStrictMode: true
+  reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
